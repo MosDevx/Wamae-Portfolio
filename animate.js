4294790e68d4828068bab846b9b732a711ad9900
MosDevx <mosesk.wamae@gmail.com>
2023-07-25 00:26:54 +0300

    add lenis

--- animate.js
+++ animate.js
@@ -67,3 +67,16 @@ homepageTL.to('.name-span',{
 // 		duration:2,
 // 		opacity:1
 // })
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
