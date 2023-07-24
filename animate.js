368d09a0f600a26b6bbd90ac654409603de7114c
MosDevx <mosesk.wamae@gmail.com>
2023-07-24 16:08:22 +0300

    fix overflow-x problem

--- animate.js
+++ animate.js
@@ -1,17 +1,29 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let t1 = gsap.timeline({
+// let t1 = gsap.timeline({
 
-	scrollTrigger:{
-		trigger:".homepage-container",
-		start:"top top",
-		end:"bottom top",
-		scrub:true,
-		pin:true,
-		anticipatePin: 1
+// 	scrollTrigger:{
+// 		// trigger:".homepage-container",
+// 		start:"top top",
+// 		end:"bottom top",
+// 		scrub:true,
+// 		pin:true,
+// 		anticipatePin: 1
 
-	}
+// 	}
+// })
+
+
+// gsap.fromTo('.homepage-h1', {x:'-50vw', opacity:0},{
+// 	scrollTrigger: '.homepage-h1',
+// 	x:0,
+// 	duration:2,
+// 	opacity:1
+// })
+
+gsap.to('.about-me-div',{
+		scrollTrigger: '.about-me-div',
+		x:300,
+		duration:2,
+		opacity:1
 })
-
-
-gsap.to('')
