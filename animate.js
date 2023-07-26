7e58f6b6714a6d5e3bbf5587a10801e7a4b21d97
MosDevx <mosesk.wamae@gmail.com>
2023-07-26 14:33:54 +0300

    homepage animation

--- animate.js
+++ animate.js
@@ -40,25 +40,79 @@ homepageTL.fromTo('.jobtitle-span', {
 '<0.6'
 );
 
-homepageTL.fromTo('.intro-text', {opacity:0,scale:0}, 
-{
-	opacity: 1,
-	duration:1.3,
-	scale:1.3,
-},
-'<2.4'
-)
+// homepageTL.fromTo('.intro-text', {opacity:0,scale:0}, 
+// {
+// 	opacity: 1,
+// 	duration:1.3,
+// 	scale:1.3,
+// },
+// '<2.4'
+// )
 
-homepageTL.to('.intro-text',{scale:1,duration:1,ease:'elastic.out(0.9,0.3)'},'<0.6')
+
+const introText = new SplitType('.intro-text',{types:'words,chars'})
+
+introText.words.forEach(word=> {
+	gsap.set(word,{
+		x:'random(-100,100)',
+		y:'random(-200,200)',
+		opacity:0
+	})
+});
+
+homepageTL.to(introText.words,{
+	x:0,
+	y:0,
+	opacity:1,
+	duration:2
+})
+
+// homepageTL.fromTo(introText.words,
+// 	{
+// 		x:"random(-100,300)",
+// 		y:"random(-20,50)",
+// 		opacity:0,
+// 		repeatRefresh:true
+// 	},
+// 	{
+// 	opacity:1,
+// 	duration:2,
+// 	ease:'elastic.out(0.9,0.3)',
+// 	// rotate:360,
+// 	x:0,
+// 	y:0,
+// 	stagger:0.1
+// },
+// '<0.6')
+
+homepageTL.to(introText.words,
+	{
+	x:"random(-300,300)",
+	y:"random(-100,400)",
+	rotation:"random(0,360)",
+	duration:4,
+	scale:'random(0.8,1.2)',
+	// stagger:0.1,
+	// yoyo:true,
+	repeat:-1,
+	repeatRefresh:true,
+	ease:"power1.inOut",
+	
+	scrollTrigger:{
+		trigger:'.intro-text',
+		markers:true,
+		start: 'top center',
+	}
+})
 
 
 gsap.to('.name-span',{
 
 	keyframes:{
-		backgroundImage:['linear-gradient(to right, rgb(236, 72, 153), rgb(239, 68, 68), rgb(234, 179, 8))','linear-gradient(to right top, rgb(217, 70, 239), rgb(220, 38, 38), rgb(251, 146, 60))','linear-gradient(to right, rgb(153, 246, 228), rgb(217, 249, 157))'],
+		backgroundImage:['linear-gradient(to right, rgb(236, 72, 153), rgb(239, 68, 68), rgb(234, 179, 8))','linear-gradient(to right top, rgb(217, 70, 239), rgb(220, 38, 38), rgb(251, 146, 60))','linear-gradient(to right, rgb(153, 246, 228), rgb(217, 249, 157))','linear-gradient(to right, rgb(255, 228, 230), rgb(204, 251, 241))','linear-gradient(to right, rgb(29, 78, 216), rgb(30, 64, 175), rgb(17, 24, 39))','linear-gradient(to right, rgb(190, 18, 60), rgb(219, 39, 119))','linear-gradient(to right, rgb(56, 189, 248), rgb(103, 232, 249))'],
 		easeEach: "slow(0.7, 0.7, false)",
 	},
-	duration:4,
+	duration:8,
 	ease: "sine.inOut",
 	yoyo:true,
 	repeat:-1
