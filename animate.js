ec350244ad2bd07f083fef03ada77d983e6573c3
MosDevx <mosesk.wamae@gmail.com>
2023-07-26 23:12:10 +0300

    split front text

--- animate.js
+++ animate.js
@@ -85,7 +85,7 @@ homepageTL.to(introText.words,{
 // },
 // '<0.6')
 
-homepageTL.to(introText.words,
+gsap.to(introText.words,
 	{
 	x:"random(-300,300)",
 	y:"random(-100,400)",
@@ -97,22 +97,59 @@ homepageTL.to(introText.words,
 	repeat:-1,
 	repeatRefresh:true,
 	ease:"power1.inOut",
-	
+	onReverseComplete:completeIntroText,
 	scrollTrigger:{
 		trigger:'.intro-text',
-		markers:true,
-		start: 'top center',
+		// markers:true,
+		start: '30 30%',
+		end:'bottom 5%',
+		toggleActions:'play reverse none none'
+
 	}
 })
 
 
-gsap.to('.name-span',{
+
+function completeIntroText(){
+	// introText.revert()
+	console.log("Called")
+	gsap.to(introText.words,{
+		x:0,
+		y:0,
+		rotation:0,
+		scale:1,
+		duration:1
+	})
+	// introText.revert()
+}
+
+
+//skew the name and title texts when scrolling down
+
+const greetingText = new SplitType('.greeting-span',{types:'words'})
+const jobTitleText = new SplitType('.jobtitle-span',{types:'words'})
+
+gsap.to([jobTitleText.words, greetingText.words],{
+
+	rotation:"random(0,360)",
+	duration: 3,
+	scrollTrigger:{
+		trigger:'.greeting-span',
+		start:'top 10%',
+		scrub:1,
+		markers:true
+	}
+
+}) 
+
+
+gsap.to('.name-span div',{
 
 	keyframes:{
 		backgroundImage:['linear-gradient(to right, rgb(236, 72, 153), rgb(239, 68, 68), rgb(234, 179, 8))','linear-gradient(to right top, rgb(217, 70, 239), rgb(220, 38, 38), rgb(251, 146, 60))','linear-gradient(to right, rgb(153, 246, 228), rgb(217, 249, 157))','linear-gradient(to right, rgb(255, 228, 230), rgb(204, 251, 241))','linear-gradient(to right, rgb(29, 78, 216), rgb(30, 64, 175), rgb(17, 24, 39))','linear-gradient(to right, rgb(190, 18, 60), rgb(219, 39, 119))','linear-gradient(to right, rgb(56, 189, 248), rgb(103, 232, 249))'],
 		easeEach: "slow(0.7, 0.7, false)",
 	},
-	duration:8,
+	duration:10,
 	ease: "sine.inOut",
 	yoyo:true,
 	repeat:-1
