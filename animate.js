54a2a4c75d7116244deb2656ef253d358f9e99ad
MosDevx <mosesk.wamae@gmail.com>
2024-11-08 03:30:55 +0300

    fix animation

--- animate.js
+++ animate.js
@@ -118,7 +118,7 @@ gsap.to(introText.words,
 
       trigger: '.intro-text',
       markers:true,
-      start: '70 48%',
+      start: '70 40%',
       // pin:true,
       end: '+=100px 60%',
       toggleActions: 'play none reset none ',
@@ -140,15 +140,12 @@ gsap.to([jobTitleText.words, greetingText.words], {
   ease: 'elastic.out(0.7,0.6)',
   scrollTrigger: {
     trigger: '.greeting-span',
-    start: 'top 20%',
-    markers: true,
-    end: 'bottom 30%',
-    scrub: 1.8,
-
-    // markers:true,
+    start: () => window.innerWidth < 768 ? 'top 20%' : 'top 10%',
+    end: () => window.innerWidth < 768 ? 'bottom 20%' : 'bottom 10%',
+    scrub: 1.5,
+    markers:true,
     // pin:true
   },
-
 });
 
 
